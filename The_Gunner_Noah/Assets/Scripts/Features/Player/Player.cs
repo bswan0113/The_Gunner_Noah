d2dc1d892e7020de5da7ccc964ac336e99341bd7
bswan0113 <116549027+bswan0113@users.noreply.github.com>
2025-11-06 23:03:04 +0900

    feat : 캐릭터 움직임

---
+++ The_Gunner_Noah/Assets/Scripts/Features/Player/Player.cs
@@ -0,0 +1,83 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+using Utils;
+
+namespace Features.Player
+{
+    public class Player : MonoBehaviour
+    {
+        [Header("Movement")]
+        public float movementSpeed;
+        public float jumpPower;
+        public LayerMask groundLayerMask;
+        public float rotationSpeed = 15f;
+
+        private Rigidbody _rigidbody;
+        private Transform cameraTransform;
+        private Vector2 curMovementInput;
+
+        [Header("Ground Check")]
+        public Transform groundCheck;
+        public float groundDistance = 0.2f;
+        private void Awake()
+        {
+            _rigidbody = GetComponent<Rigidbody>();
+            cameraTransform = Camera.main.transform;
+        }
+
+        private void FixedUpdate()
+        {
+            MovePlayer();
+            RotatePlayer();
+        }
+
+        private void MovePlayer()
+        {
+            Vector3 camForward = cameraTransform.forward;
+            Vector3 camRight = cameraTransform.right;
+
+            camForward.y = 0;
+            camRight.y = 0;
+
+            camForward.Normalize();
+            camRight.Normalize();
+
+            Vector3 moveDirection = (camForward * curMovementInput.y + camRight * curMovementInput.x).normalized;
+
+            Vector3 finalVelocity = moveDirection * movementSpeed;
+            finalVelocity.y = _rigidbody.velocity.y;
+
+            _rigidbody.velocity = finalVelocity;
+        }
+
+        private void RotatePlayer()
+        {
+            Vector3 lookDirection = cameraTransform.forward;
+            lookDirection.y = 0;
+
+            if (lookDirection != Vector3.zero)
+            {
+                Quaternion targetRotation = Quaternion.LookRotation(lookDirection);
+                transform.rotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.fixedDeltaTime);
+            }
+        }
+
+        public void OnMove(InputAction.CallbackContext context)
+        {
+            curMovementInput = context.ReadValue<Vector2>();
+        }
+
+        public void OnJump(InputAction.CallbackContext context)
+        {
+            if (context.phase == InputActionPhase.Performed && IsGrounded())
+            {
+                _rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);
+            }
+        }
+
+        bool IsGrounded()
+        {
+            return Physics.CheckSphere(groundCheck.position, groundDistance, groundLayerMask);
+        }
+    }
+}
